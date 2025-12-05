19134fbe7bb1c7ecb86fa5105d6b63dc6bd8655c
Pedro Gambini <pedrogambini1961@gmail.com>
2025-12-05 02:09:48 -0300

    Alteração do nome da classe, de "Bnco" para "Banco".

--- src/DesafioBancoVirtual.java
+++ src/DesafioBancoVirtual.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DesafioBnacoVirtual {
+public class DesafioBancoVirtual {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int escolha = 0;
